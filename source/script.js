b76fba1a8edffb7b9caea870e36e05727b0f5b28
Olga Peganova <volha25121987@gmail.com>
2023-01-13 15:13:56 +0700

    adaptive updated

--- source/script.js
+++ source/script.js
@@ -15,3 +15,12 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+let nav = document.querySelector(".nav"),
+    toggle = document.querySelector(".nav__toggle");
+nav.classList.remove("nav--nojs"),
+nav.classList.remove("nav--opened"),
+nav.classList.add("nav--closed"),
+toggle.addEventListener("click", (function() {
+    nav.classList.contains("nav--opened") ? (nav.classList.remove("nav--opened"), nav.classList.add("nav--closed")) : (nav.classList.remove("nav--closed"), nav.classList.add("nav--opened"))
+}));
