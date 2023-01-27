3034f3faafa65697a7d27a63d920b69ee9781fc2
Olga Peganova <volha25121987@gmail.com>
2023-01-27 13:44:44 +0700

    webp added

--- source/js/script.js
+++ source/js/script.js
@@ -5,10 +5,10 @@ let navToggleNoJs = document.querySelector('.main-header__toggle--nojs');
 navToggle.classList.remove("main-header__toggle--nojs");
 navMain.classList.add('main-nav--closed');
 navMain.classList.remove('main-nav--opened');
+navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
   navToggle.classList.toggle("active");
-  navMain.classList.remove('main-nav--nojs');
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
